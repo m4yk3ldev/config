cf22ab4e140c669ee63d6676de113741cd4c8e90
Maykel Arias Torres <dev0006@simelo.tech>
2021-11-15 08:53:15 -0500

    Agregando configuraciones de npm yarn

--- install.sh
+++ install.sh
@@ -4,8 +4,11 @@
 set -ex
 
 echo "Enviando enlace .vimrc"
-ln -sf /home/adminred/config-vim/.vimrc ~/.vimrc
+ln -sf .vimrc ~/.vimrc
 
 echo "Enviando enlace  a .vim"
-ln -sf /home/adminred/config-vim/.vim ~/.vim
+ln -sf .vim ~/.vim
+
+echo "Agregando configuraciones al zsh"
+cat .zshrc >> ~/.zshrc
 
