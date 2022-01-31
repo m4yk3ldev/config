e4a0d0fa24a33ff7574892b3defd6a44484e4f5c
Maykel Arias Torres <hanibal920915@gmail.com>
2022-01-31 12:01:51 -0500

    Agregando keymaps a LSP

--- nvim/lua/user/lsp/init.lua
+++ nvim/lua/user/lsp/init.lua
@@ -1,22 +1,20 @@
 local status_ok, _ = pcall(require, "lspconfig")
 if not status_ok then
 	return
- end
+end
 
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-  vim.lsp.diagnostic.on_publish_diagnostics, {
-    underline = true,
-    -- This sets the spacing and the prefix, obviously.
-    virtual_text = {
-      spacing = 4,
-      prefix = ''
-    }
-  }
-)
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+	underline = true,
+	-- This sets the spacing and the prefix, obviously.
+	virtual_text = {
+		spacing = 4,
+		prefix = "",
+	},
+})
 
-require                       "user.lsp.installer"
+require("user.lsp.installer")
 require("user.lsp.handlers").setup()
 require("user.lsp.colors")
 require("user.lsp.config")
 require("user.lsp.null-ls")
-
+require("user.lsp.mapping")
