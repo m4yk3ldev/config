99a2afdf2a9914ee57fc71d8fc05677e360341a8
Maykel Arias Torres <hanibal920915@gmail.com>
2022-02-10 23:13:38 -0500

    Agregando mas conf de lsp

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,7 +2,6 @@ require("impatient") -- This needs to be first
 require("user.config") -- Para cargar todas las configuraciones básicas
 require("user.keymap") -- Cargando keyboard principales
 require("user.plug") -- Cargando los plugins
-require("user.packer_cmd") -- Para cargar todas las configuraciones packer_cmd
 require("user.lsp") -- Cargando Language Serve
 require("user.theme") -- Cargando los theme
 require("user.plugins") -- Cargando los theme
