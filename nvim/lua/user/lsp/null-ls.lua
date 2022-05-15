bac58e85974f91e1d045b98c6012a5d3765d6fb9
Maykel Arias Torres <hanibal920915@gmail.com>
2022-05-15 15:08:09 -0400

    Comment completation

--- nvim/lua/user/lsp/null-ls.lua
+++ nvim/lua/user/lsp/null-ls.lua
@@ -10,7 +10,7 @@ local diagnostics = null_ls.builtins.diagnostics
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/code_actions
 local code_actions = null_ls.builtins.code_actions
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/completion
-local completion = null_ls.builtins.completion
+-- local completion = null_ls.builtins.completion
 
 null_ls.setup({
 	debug = false,
