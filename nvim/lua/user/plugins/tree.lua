c692af7013e1b203361e2d5d4801659bfe5fb3ef
Maykel <hanibal920915@gmail.com>
2022-06-19 01:44:29 -0400

    Corrected position NeoTree to side right

--- nvim/lua/user/plugins/tree.lua
+++ nvim/lua/user/plugins/tree.lua
@@ -53,10 +53,10 @@ nvim_tree.setup({
 		timeout = 500,
 	},
 	view = {
-		width = 30,
+		width = 50,
 		height = 30,
 		hide_root_folder = false,
-		side = "left",
+		side = "right",
 		mappings = {
 			custom_only = false,
 			list = {
