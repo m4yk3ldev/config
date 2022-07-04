2ff5ae2c0fc45334916db5899cd3a82cbe42da31
Maykel <hanibal920915@gmail.com>
2022-07-04 18:16:56 -0400

    Solo sonokai

--- nvim/lua/user/theme.lua
+++ nvim/lua/user/theme.lua
@@ -1,21 +1,5 @@
--- -- Example config in Lua
--- vim.g.tokyonight_style = "storm"
--- vim.g.tokyonight_italic_functions = true
--- vim.g.tokyonight_italic_comments = true
--- vim.g.tokyonight_sidebars = { "qf", "vista_kind", "terminal", "packer" }
--- vim.o.background = "dark"
--- vim.g.tokyonight_transparent_sidebar = true
--- vim.g.tokyonight_terminal_colors = true
---
--- -- Change the "hint" color to the "orange" color, and make the "error" color bright red
--- vim.g.tokyonight_colors = { hint = "orange", error = "#ff0000" }
--- vim.g.tokyonight_lualine_bold = true
--- -- vim.g.tokyonight_transparent_sidebar = true
--- -- -- Load the colorscheme
--- vim.cmd([[colorscheme tokyonight]])
--- --
 vim.opt.termguicolors = true
--- Para Sonokai
+vim.o.background = "dark"
 vim.g.sonokai_style = 'andromeda'
 vim.g.sonokai_better_performance = 1
 vim.g.sonokai_enable_italic = 1
