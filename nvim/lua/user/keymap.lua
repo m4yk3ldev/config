866c3fc9614745445f4d87b4c0ce0114c908b0b0
Maykel <hanibal920915@gmail.com>
2022-07-04 16:10:26 -0400

    Eliminado olds keymaps

--- nvim/lua/user/keymap.lua
+++ nvim/lua/user/keymap.lua
@@ -87,23 +87,6 @@ keymap("n", "<leader>gs", "<cmd>Telescope git_status<CR>", opts)
 keymap("n", "<leader>gc", "<cmd>Telescope git_commits<CR>", opts)
 keymap("n", "<leader>h", "<cmd>Telescope oldfiles<CR>", opts)
 
--- LSP
--- keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
--- keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
--- keymap("n", "<f3>", ":Format<cr>", opts)
--- keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
--- keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
--- keymap("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
--- keymap("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
--- keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
--- keymap("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
--- keymap("n", "[d", '<cmd>lua vim.diagnostic.goto_prev({ border = "rounded" })<CR>', opts)
--- keymap("n", "gl", '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics({ border = "rounded" })<CR>', opts)
--- keymap("n", "]d", '<cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>', opts)
--- keymap("n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
--- vim.cmd([[ command! Format execute 'lua vim.lsp.buf.formatting()' ]])
--- vim.cmd([[ command! FormatOnSave execute 'lua vim.lsp.buf.formatting_on_save()' ]])
-
 -- Para sellecionar todo
 keymap("n", "<C-a>", "ggVG", opts)
 -- COC
