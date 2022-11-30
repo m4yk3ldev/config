2a45473f42f0dad0c87cdbc9b98d8a9a44cbc1e9
Maykel Arias Torres <hanibal920915@gmail.com>
2022-11-30 12:28:06 -0300

    Adding new pacato xfce-screenshot

--- install-gui.sh
+++ install-gui.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Instalando dependencias de Trizen"
-trizen -S rofi wezterm wget nerd-fonts-complete arandr i3status-rust i3-scrot spotify-tui
+trizen -S rofi wezterm wget nerd-fonts-complete arandr i3status-rust i3-scrot spotify-tui-bin xfce4-screenshooter  
 
 echo "Agregando configuraciones de i3"
 mkdir -p $HOME/.i3
