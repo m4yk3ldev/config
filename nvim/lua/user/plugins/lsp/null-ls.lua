2e9e588f2018e9bd7d6a9b8d5ee61d1e99794657
Maykel Arias Torres <hanibal920915@gmail.com>
2022-12-11 22:45:08 -0300

    Adding null-ls

--- nvim/lua/user/plugins/lsp/null-ls.lua
+++ nvim/lua/user/plugins/lsp/null-ls.lua
@@ -14,12 +14,8 @@ end
 
 null_ls.setup {
   sources = {
-    null_ls.builtins.formatting.prettierd,
     null_ls.builtins.formatting.autopep8,
     null_ls.builtins.formatting.djhtml,
-    null_ls.builtins.diagnostics.eslint_d.with({
-      diagnostics_format = '[eslint] #{m}\n(#{c})'
-    }),
     null_ls.builtins.diagnostics.djlint,
   },
   on_attach = function(client, bufnr)
