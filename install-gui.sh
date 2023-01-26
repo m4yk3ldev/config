cfa0fd9538ed3a00dc1389107d67cfbd5551c2ec
Maykel Arias Torres <hanibal920915@gmail.com>
2023-01-26 15:56:48 -0300

    remove i3

--- install-gui.sh
+++ install-gui.sh
@@ -1,34 +1,18 @@
 #!/bin/bash
 echo "Instalando dependencias de Yay"
-yay -S rofi wget nerd-fonts-complete arandr i3status-rust i3-scrot spotify-tui-bin xfce4-screenshooter kitty exa
-
-echo "Agregando configuraciones de i3"
-mkdir -p $HOME/.i3
-ln -sfv $PWD/.i3/config $HOME/.i3/config
+yay -S rofi wget nerd-fonts-complete xfce4-screenshooter kitty exa
 
 echo "Agregando configuraciones al zsh"
-ln -sfv $PWD/.zshrc  $HOME/.zshrc
+ln -sfv "$PWD"/.zshrc  "$HOME/.zshrc"
 
 
 echo "Agregando configuraciones al zsh"
-ln -sfv $PWD/.p10k.zsh  $HOME/.p10k.zsh
-
-echo "Agregando configuraciones de i3status"
-ln -sfv $PWD/.i3/theme-i3status.toml $HOME/.i3/theme.toml
-
+ln -sfv "$PWD/.p10k.zsh"  "$HOME/.p10k.zsh"
 
 echo "Agregando configuraciones de Rofi"
-mkdir -p $HOME/.config/rofi/
-ln -sfv $PWD/rofi/config.rasi $HOME/.config/rofi/config.rasi
-
-echo "Agregando configuraciones de VolumeIco"
-mkdir -p $HOME/.config/volumeicon
-ln -sfv $PWD/volumeicon/volumeicon $HOME/.config/volumeicon/volumeicon
-
-echo "Agregando configuraciones de WezTerm"
-mkdir -p $HOME/.config/wezterm/
-ln -sfv $PWD/wezterm/wezterm.lua $HOME/.config/wezterm/wezterm.lua
+mkdir -p "$HOME/.config/rofi/"
+ln -sfv "$PWD/rofi/config.rasi" "$HOME/.config/rofi/config.rasi"
 
 echo "Agregando configuraciones de Kitty"
-mkdir -p $HOME/.config/kitty/
-ln -sfv $PWD/kitty/kitty.conf $HOME/.config/kitty/kitty.conf
+mkdir -p "$HOME/.config/kitty/"
+ln -sfv "$PWD/kitty/kitty.conf" "$HOME/.config/kitty/kitty.conf"
